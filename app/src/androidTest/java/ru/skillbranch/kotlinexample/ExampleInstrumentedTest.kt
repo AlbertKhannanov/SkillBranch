ac4af3ad73c5a48c7b23efd609465b0fbbca0839
Albert <xannanov.albert@mail.ru>
2021-08-25 15:34:34 +0400

    ready

--- app/src/androidTest/java/ru/skillbranch/kotlinexample/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/ru/skillbranch/kotlinexample/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package ru.skillbranch.gameofthrones
+package ru.skillbranch.kotlinexample
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
---
+++ app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
@@ -0,0 +1,53 @@
+package ru.skillbranch.kotlinexample
+
+import android.util.Log
+import androidx.annotation.VisibleForTesting
+
+object UserHolder {
+    private val map = mutableMapOf<String, User>()
+
+    fun registerUser(
+        fullName: String,
+        email: String,
+        password: String
+    ): User = User.makeUser(fullName, email = email, password = password)
+        .let { user ->
+            if (map.contains(user.login))
+                throw IllegalArgumentException("A user with this email already exists")
+            map[user.login] = user
+            user
+        }
+
+    fun loginUser(login: String, password: String): String? =
+        map[login.trim()]?.let {
+            if (it.checkPassword(password) || it.accessCode == password) it.userInfo
+            else null
+        } ?: map[login.replace("""[^+\d]""".toRegex(), "")]?.let {
+            if (it.checkPassword(password) || it.accessCode == password) it.userInfo
+            else null
+        }
+
+    fun registerUserByPhone(fullName: String, phone: String): User =
+        User.makeUser(fullName, phone = phone).let { user ->
+            if (map.contains(user.login))
+                throw IllegalArgumentException("A user with this phone already exists")
+            if (!user.phone?.matches("""^([+])([0-9]{11})$""".toRegex())!!)
+                throw IllegalArgumentException("Enter a valid phone number starting with a + and containing 11 digits")
+            map[user.login] = user
+            user
+        }
+
+
+    fun requestAccessCode(login: String) {
+        map[login.replace("""[^+\d]""".toRegex(), "")]?.let { user ->
+            val newCode = user.generateAccessCode()
+            user.changePassword(user.accessCode!!, newCode)
+            user.accessCode = newCode
+        }
+    }
+
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    fun clearHolder() {
+        map.clear()
+    }
+}
